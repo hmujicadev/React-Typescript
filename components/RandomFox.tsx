c835002c94a540e8fe2d3cef27b325fcfaac1679
hmujicadev <hmujicadev@gmail.com>
2023-03-22 21:18:44 -0300

    adding types to useRef and initial value

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,6 +1,10 @@
+import { useRef } from "react"
+
 type Props = {image:string}
 
 //Explicit mode to create a component in typescript (Recomended Method)
 export const RandomFox = ({image}:Props):JSX.Element => {
-  return <img src={image} width={320} height='auto' className={'rounded'}/>
+  const node = useRef<HTMLImageElement>(null)
+
+  return <img ref={node} src={image} width={320} height='auto' className={'rounded'}/>
 }

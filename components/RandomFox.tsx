28b2034161bf7f3142e072b6fbe524de4c439b9f
hmujicadev <hmujicadev@gmail.com>
2023-03-23 20:46:28 -0300

    adding observable

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,10 +1,36 @@
-import { useRef } from "react"
+import { useRef, useEffect, useState } from "react"
 
 type Props = {image:string}
 
 //Explicit mode to create a component in typescript (Recomended Method)
 export const RandomFox = ({image}:Props):JSX.Element => {
   const node = useRef<HTMLImageElement>(null)
+  const [src, setsrc] = useState(
+  "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
+  )
 
-  return <img ref={node} src={image} width={320} height='auto' className={'rounded'}/>
+  useEffect(() => {
+    //nuevo observador
+    //onIntersection - > console.log()
+    const observer = new IntersectionObserver((entries)=>{
+      entries.forEach( entry => {
+        if(entry.isIntersecting){
+           setsrc(image);
+        } 
+      })
+    })
+ 
+    // observe node
+    if(node.current){
+      observer.observe(node.current)
+    }
+    
+    // Desconectar
+    return () =>{
+      observer.disconnect()
+    }
+  }, [image])
+
+  return <img ref={node} src={src} width={320} height='auto' className='rounded bg-gray-300' />
+            
 }
